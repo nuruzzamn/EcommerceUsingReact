c6d2a281768f799edf57f3f3e055042399778192
unknown <nur.mnzaman@gmail.com>
2023-11-30 15:51:14 +0600

    [feat] route configure

--- src/components/Eshop.jsx
+++ src/components/Eshop.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Shop from '../pages/Shop'
+import ShopCategory from '../pages/ShopCategory'
 
 const Eshop = () => {
   return (
     <div>
+      <BrowserRouter>
         <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
+          <Route path='/men' element={<ShopCategory category="men"/>}></Route>
+          <Route path='/women' element={<ShopCategory category="women"/>}></Route>
+          <Route path='/kids' element={<ShopCategory category="kids"/>}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
