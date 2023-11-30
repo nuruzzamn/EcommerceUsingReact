60559a6f523db3b3a29aca6f40f965d694bfe768
unknown <nur.mnzaman@gmail.com>
2023-11-30 12:42:45 +0600

    [feat] Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import cartIcom from '../assets/cart_icon.png'
 const Navbar = () => {
+
+  const [menu, setMenu] = useState('shop')
+
   return (
     <div className='flex justify-around p-[16px] shadow-sm '>
         <div className='flex items-center gap-3'>
@@ -10,16 +13,24 @@ const Navbar = () => {
         </div>
 
         <ul className='flex items-center list-none gap-12 text-lg text-slate-700 font-medium'>
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer'>Shop 
-            <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/></li>
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer'>Men</li>
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer'>Women</li>
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer'>Kids</li>
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("shop")}}>Shop{menu === "shop" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            </li>
+
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("men")}}>Men{menu === "men" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            </li>
+
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("women")}}>Women{menu === "women" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            </li>
+
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("kids")}}>kids{menu === "kids" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            </li>
+
         </ul>
 
         <div className='flex items-center gap-11'>
-            <button className='w-36 h-14 outline-none rounded-[75px] font-medium text-xl border border-gray-400 bg-white cursor-pointer'>Login</button>
+            <button className='w-36 h-14 outline-none rounded-[75px] font-medium text-xl border border-gray-400 bg-white cursor-pointer active:bg-[#f3f3f3] '>Login</button>
             <img src={cartIcom} alt="" />
+            <div className='w-[22px] h-[22px] flex justify-center items-center mt-[-35px] ml-[-55px] rounded-[12px] text-sm bg-red-500 text-white'>0</div>
         </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // BACKGROUND COLORS
+        // Light
+        colorWhite: '#fffef7',
+      }
+    },
   },
   plugins: [],
 }
