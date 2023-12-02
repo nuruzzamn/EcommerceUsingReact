e29f34b8897a945f227018c1b4e0f71a0a49f526
unknown <nur.mnzaman@gmail.com>
2023-12-02 19:03:45 +0600

    [feat] route configure

--- src/components/Eshop.jsx
+++ src/components/Eshop.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
 import Shop from '../pages/Shop'
 import ShopCategory from '../pages/ShopCategory'
+import Products from '../pages/Products'
+import Cart from '../pages/Cart'
+import LoginSignup from '../pages/LoginSignup'
 
 const Eshop = () => {
   return (
@@ -15,6 +18,13 @@ const Eshop = () => {
           <Route path='/men' element={<ShopCategory category="men"/>}></Route>
           <Route path='/women' element={<ShopCategory category="women"/>}></Route>
           <Route path='/kids' element={<ShopCategory category="kids"/>}></Route>
+
+          <Route path='/product' element={<Products/>}> 
+              <Route path=':productId' element={<Products/>}> </Route>
+          </Route>
+
+          <Route path='/cart' element={<Cart/>}></Route>
+          <Route path='/login' element={<LoginSignup/>}></Route>
 
         </Routes>
       </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../assets/logo.png'
 import cartIcom from '../assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
 
   const [menu, setMenu] = useState('shop')
@@ -13,23 +14,29 @@ const Navbar = () => {
         </div>
 
         <ul className='flex items-center list-none gap-12 text-lg text-slate-700 font-medium'>
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("shop")}}>Shop{menu === "shop" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("shop")}}> <Link to='/'>Shop</Link>
+            {menu === "shop" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
             </li>
 
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("men")}}>Men{menu === "men" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("men")}}><Link to='/men'>Men</Link>
+            {menu === "men" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
             </li>
 
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("women")}}>Women{menu === "women" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("women")}}><Link to='/women'>Women</Link>
+            {menu === "women" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
             </li>
 
-            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("kids")}}>kids{menu === "kids" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
+            <li className='flex flex-col items-center justify-center gap-1 cursor-pointer' onClick={()=>{setMenu("kids")}}><Link to='/kids'>kids</Link>
+            {menu === "kids" ? <hr className='border-none w-[80%] h-1 rounded-xl bg-red-400'/> : <></>} 
             </li>
 
         </ul>
 
         <div className='flex items-center gap-11'>
-            <button className='w-36 h-14 outline-none rounded-[75px] font-medium text-xl border border-gray-400 bg-white cursor-pointer active:bg-[#f3f3f3] '>Login</button>
-            <img src={cartIcom} alt="" />
+
+            <Link to='/login'><button className='w-36 h-14 outline-none rounded-[75px] font-medium text-xl border border-gray-400 bg-white cursor-pointer active:bg-[#f3f3f3] '>Login</button></Link>
+            
+            <Link to='/cart'><img src={cartIcom} alt="" className='cursor-pointer'/></Link>
             <div className='w-[22px] h-[22px] flex justify-center items-center mt-[-35px] ml-[-55px] rounded-[12px] text-sm bg-red-500 text-white'>0</div>
         </div>
     </div>
